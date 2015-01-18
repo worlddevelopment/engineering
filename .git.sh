42c816228998e34a2d2b192c0c8af1d12212a11d
faerietree <radagast@dragontale.de>
2015-01-18 23:43:40 +0100

    Merge basic pressure gradient calculations.

--- .git.sh
+++ .git.sh
@@ -6,27 +6,42 @@ if [ -z $1 ]; then
 	
 fi
 
-FILE='./engineering.ods'
+FILE='engineering.ods'
+
+# git add didn't like the paths of ls.
+#all_but_file=$(ls | grep '[^.]*' | awk -F'engineering.ods' '{print $1 $2}')
+# maybe it likes find:
+#all_but_file=$(find . | sed 's/[.][/][.].*//' | awk -F'engineering.ods' '{print $1 $2}')
+
+#all_but_file=eval "find . -not -path '*/\.*' | awk -F'engineering.ods' '{print $1 $2}'"
+#all_but_file=eval "find . -not -path '*engineering.ods'"
+#all_but_file=$(find . -not -path '*engineering.ods' -not -path '*/.*' | sed 's/[ ]/\\ /g')
+CMD_git_add_all_but_hidden_and_file="find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -print0 | xargs -0 git add" # <-- -print0 to handle spaces, e.g. Object xY.
+
+#echo $all_but_file
 
 if [ $1 == 'add' ]; then
     ./.unpack.sh
 	echo 'Adding ...'
 	if [ -z $2 ]; then
 	    echo 'Adding all non-hidden files ...'
-	    git add [^.]*
+		#git add $all_but_file
+		eval $CMD_git_add_all_but_hidden_and_file
 	else:
         git add $2 
     fi
 elif [ $1 == 'commit' ]; then
     ./.unpack.sh
 	echo 'Adding all the unpacked files ...'
-	git add [^.]*
+	#git add $all_but_file
+	eval $CMD_git_add_all_but_hidden_and_file
 	echo 'Committing ...'
     CMD='git commit '
-	if [ -z $3 && $3 == '-m' ]; then
-	    CMD=$CMD''$2' '$3' "'$4'"'
-	else:
-	    CMD=$CMD''$2' "'$3'"'
+	#if [[ ${2#*-}=='a' ]]; then
+	if [ ${2} == "-a" ]; then
+	    CMD=$CMD' '$2' '$3' "'$4'"'
+	elif [ ${2} == "-m" ]; then
+	    CMD=$CMD' '$2' "'$3'"'
 	fi
 	echo $CMD
 	eval $CMD
--- .unpack.sh
+++ .unpack.sh
@@ -6,9 +6,10 @@ if [ -f $FILE ]; then
     echo 'Unpacking ...'
     unzip $FILE
 	echo '*done*'
-    echo 'Moving (backing up) to '$FILE''$BACKUP_EXT'...'
-    mv -i $FILE $FILE''$BACKUP_EXT
-	echo '*done*'
+	# If it just has been unpacked, then it's still up to date unless manual changes are applied, but then it's the responsibility of the developer to integrate these manual changes by terminating the program properly before editing and recreating the file using .git.sh pull or similar.
+    #echo 'Moving (backing up) to .'$FILE''$BACKUP_EXT'...'
+    #mv -i $FILE '.'$FILE''$BACKUP_EXT
+	#echo '*done*'
 elif [ ! -f $FILE ]; then
     echo $FILE' not found. => Manual changes or already backed up. => No unpacking required.'
 fi
