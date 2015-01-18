d299c853a7a231b1a61d892975c549b30a262c65
faerietree <radagast@dragontale.de>
2015-01-19 00:45:40 +0100

    Really prevent hidden files and built calculator from being staged for a commit.

--- .git.sh
+++ .git.sh
@@ -8,35 +8,39 @@ fi
 
 FILE='engineering.ods'
 
-# git add didn't like the paths of ls.
+# git add didn't like the paths of ls:
 #all_but_file=$(ls | grep '[^.]*' | awk -F'engineering.ods' '{print $1 $2}')
-# maybe it likes find:
+
+# nor those of find without -print0:
 #all_but_file=$(find . | sed 's/[.][/][.].*//' | awk -F'engineering.ods' '{print $1 $2}')
+#all_but_file=eval "find . -not -path '*/\.*' -not -path '..' -not -path '.'"
 
-#all_but_file=eval "find . -not -path '*/\.*' | awk -F'engineering.ods' '{print $1 $2}'"
-#all_but_file=eval "find . -not -path '*engineering.ods'"
-#all_but_file=$(find . -not -path '*engineering.ods' -not -path '*/.*' | sed 's/[ ]/\\ /g')
+##
+# WORKING ALTERNATIVE -print0
 #CMD_git_add_all_but_hidden_and_file="find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -print0 | xargs -0 git add " # <-- -print0 to handle spaces, e.g. Object xY.
+#grep --null '[^.]*'
+#-exec git add "{}" \;
 
-#echo $all_but_file
+##
+# WORKING ALTERNATIVE without -print0 (the one that currently is used, credit oyouareatubeo from stackoverflow)
+#http://stackoverflow.com/questions/143171/how-can-i-use-xargs-to-copy-files-that-have-spaces-and-quotes-in-their-names
+#find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -not -path '..' -not -path '.' | sed 's/.*/"&"/' | xargs git add 
+
+# NOTE:
+# => Now still not using subdirectories because even if a custom OpenFOAM or other engineering tools shall be integrated, the integration will rather be the other way, i.e. this engineeringtoolkit repository will be loaded as submodule or subtree in OpenFOAM.
 
 if [ $1 == 'add' ]; then
     ./.unpack.sh
-	echo 'Adding ...'
+	echo 'Adding all the unpacked files ...'
 	if [ -z $2 ]; then
 	    echo 'Adding all non-hidden files ...'
 		#git add $all_but_file
 		#$CMD_git_add_all_but_hidden_and_file
-		find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -print0 | xargs -0 git add 
-	else:
+		$(find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -not -path '..' -not -path '.' | sed 's/.*/"&"/' | xargs git add)
+	else
         git add $2 
     fi
 elif [ $1 == 'commit' ]; then
-    ./.unpack.sh
-	echo 'Adding all the unpacked files ...'
-	#git add $all_but_file
-	#$CMD_git_add_all_but_hidden_and_file
-	find . -not -path '*engineering.ods' -not -path '*/.*' -not -path '*.~*' -print0 | xargs -0 git add 
 	echo 'Committing ...'
     CMD='git commit '
 	#if [[ ${2#*-}=='a' ]]; then
